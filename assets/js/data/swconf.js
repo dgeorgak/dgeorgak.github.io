9d99be34fe534797e6cc75f84e1a6d10ed7bf3f2
dgeorgak <dgeorgak@users.noreply.github.com>
2024-10-17 14:22:29 +0000

    deploy: e9a74f970ef1b955fe5efaf24569c717e9df5a26

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1729174949',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
