3710d6e7b13cac56301a00a85a2977ca610ced46
Dim <dimgeorgakopoulos@gmail.com>
2024-10-18 10:53:22 +0300

    change avatar

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729174005',resources: [
+    cacheName: 'chirpy-1729175600',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
