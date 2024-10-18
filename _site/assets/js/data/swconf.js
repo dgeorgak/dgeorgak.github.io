dfa7d30df5bef77dc863562fd37ef4ddd4115ca9
Dim <dimgeorgakopoulos@gmail.com>
2024-10-18 12:47:10 +0300

    dark-light toggle on, social media icons configured, favicon & avatar added

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729175600',resources: [
+    cacheName: 'chirpy-1729244706',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
