19e0935d2d23a0dd5b1bbffc107d81fb3bd30842
Dim <dimgeorgakopoulos@gmail.com>
2024-10-18 13:57:28 +0300

    remove light toggle

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729244706',resources: [
+    cacheName: 'chirpy-1729245550',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
