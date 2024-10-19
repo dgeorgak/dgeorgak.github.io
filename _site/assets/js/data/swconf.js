34b07c48f5ffe56a7fda8c36b83f23061c937c6c
Dim <dimgeorgakopoulos@gmail.com>
2024-10-19 22:19:24 +0300

    fix light toggle & search bar. take 3

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729354889',resources: [
+    cacheName: 'chirpy-1729365502',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
